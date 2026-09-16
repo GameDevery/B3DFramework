df2d42eef8eb4db8ed84d9c1f7623338bb539805
Marko Pintera <bearishsun@gmail.com>
2026-09-16 10:47:24 +0200

    Fixed: Removed unecessary std::move

--- Source/Engine/Core/Resources/B3DPackageManager.cpp
+++ Source/Engine/Core/Resources/B3DPackageManager.cpp
@@ -84,7 +84,7 @@ TUnique<PackageReadLock> PackageManager::LoadOrGetPackage(const Path& packagePhy
 	const AcquirePackageLockResult lockResult = AcquireReadLock(packagePhysicalPath, readLockOptions, readLock);
 
 	if(lockResult == AcquirePackageLockResult::Acquired && readLock != nullptr)
-		return std::move(readLock);
+		return readLock;
 
 	return nullptr;
 }
